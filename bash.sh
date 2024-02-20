090daf0acb724e170d6b915f46d9254f77481c30
ishita301 <ishita.imscit21@gmail.com>
2024-02-20 16:18:50 +0530

    first commit

--- bash.sh
+++ bash.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
-OLD_EMAIL="himanshu.dudhat@bacancytechnology.com"
-CORRECT_NAME="Ishita301"
+OLD_EMAIL="mihirkanzariya@Mihirs-MacBook-Pro.local"
+CORRECT_NAME="ishita301"
 CORRECT_EMAIL="ishita.imscit21@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
 ' --tag-name-filter cat -- --branches --tags
